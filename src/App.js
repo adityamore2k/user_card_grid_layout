64c6f7c0d19fb4feff82820757d7a9de12f116a2
Aditya More <62210603+adityamore2k@users.noreply.github.com>
2022-05-30 23:59:28 +0530

    show/hide users on button click Show and hide users on button click

--- src/App.js
+++ src/App.js
@@ -5,27 +5,45 @@ import { useState, useEffect } from "react";
 
 export default function App() {
   // useEffect hook to make HTTP request
-
   // Array of user objects
   const [users, setUsers] = useState([]);
+  const [buttonClick, setButtonClick] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
+    // Logic to show loader icon
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
     fetch("https://reqres.in/api/users?page=1")
       .then((response) => response.json())
-      .then((data) => setUsers(data.data));
+      .then(function (data) {
+        setUsers(data.data);
+      });
   }, []);
 
-  const user_cards = users.map((user) => (
-    <User
-      avatar={user.avatar}
-      first_name={user.first_name}
-      last_name={user.last_name}
-      email={user.email}
-    />
-  ));
+  function returnUsers() {
+    setButtonClick((prevVal) => !prevVal);
+  }
   return (
     <div className="App">
-      <Navbar />
-      {user_cards}
+      {/* <Navbar /> */}
+      <button onClick={returnUsers}>Get users</button>
+      {buttonClick ? (
+        isLoading ? (
+          <h1>Loading</h1>
+        ) : (
+          users.map((user) => (
+            <User
+              avatar={user.avatar}
+              first_name={user.first_name}
+              last_name={user.last_name}
+              email={user.email}
+            />
+          ))
+        )
+      ) : (
+        <h1>Click to see users</h1>
+      )}
     </div>
   );
 }
